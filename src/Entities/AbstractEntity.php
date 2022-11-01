67101a528abef835d7c793a754bc25f80dee25ba
tfleury <t.fleury@gammasoftware.fr>
2022-11-01 19:17:07 +0100

    Initialisation du projet github

---
+++ src/Entities/AbstractEntity.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * CAG - Clean Architecture Generator
+ *
+ * Tristan Fleury <http://viduc.github.com/>
+ *
+ * Licence: GPL v3 https://opensource.org/licenses/gpl-3.0.html
+ */
+
+namespace Cag\Entities;
+
+abstract class AbstractEntity
+{
+    protected $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
---
+++ src/Entities/Structure.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * CAG - Clean Architecture Generator
+ *
+ * Tristan Fleury <http://viduc.github.com/>
+ *
+ * Licence: GPL v3 https://opensource.org/licenses/gpl-3.0.html
+ */
+
+namespace Cag\Entities;
+
+class Structure extends AbstractEntity
+{
+
+
+}
