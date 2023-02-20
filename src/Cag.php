cab08c39493b23bfc6aeac1f055e21a230be021b
t.fleury <t.fleury@gammasoftware.fr>
2023-02-20 09:06:57 +0100

    refacto

--- src/Cag.php
+++ src/Cag.php
@@ -10,24 +10,37 @@ declare(strict_types=1);
 
 namespace Cag;
 
+use Cag\Containers\DependencyInjection;
 use Cag\Containers\DependencyInjectionInterface;
+use Cag\Containers\Exceptions\ComposerException;
+use Cag\Containers\Exceptions\DefinitionException;
 use Cag\Containers\Exceptions\DependencyInjectionException;
 use Cag\Containers\Exceptions\NotFoundException;
 use Cag\Presenters\PresenterInterface;
 use Cag\Requests\RequestInterface;
 use Cag\UseCases\UseCaseInterface;
 use Cag\UseCases\UseCaseNameExtenderAbstract;
+use ReflectionException;
 
 class Cag implements UseCaseInterface
 {
     /**
-     * @var DependencyInjectionInterface|null
+     * @var DependencyInjectionInterface
      */
-    private ?DependencyInjectionInterface $dependencyInjection;
+    private DependencyInjectionInterface $dependencyInjection;
+
+    /**
+     * @throws ComposerException
+     * @throws DefinitionException
+     * @throws ReflectionException
+     * @throws NotFoundException
+     */
     public function __construct(
-        ?DependencyInjectionInterface $dependencyInjection = null
+        ?DependencyInjectionInterface $externalDependencyInjection = null
     ) {
-        $this->dependencyInjection = $dependencyInjection;
+        $this->dependencyInjection = new DependencyInjection(
+            $externalDependencyInjection
+        );
     }
 
 
