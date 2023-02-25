768bf1fadb62bd3525bd67f7ee69142030f1956e
t.fleury <t.fleury@gammasoftware.fr>
2023-02-25 15:41:04 +0100

    refacto

--- app/Containers/DependencyServiceProvider.php
+++ app/Containers/DependencyServiceProvider.php
@@ -11,17 +11,6 @@ declare(strict_types=1);
 
 namespace App\Containers;
 
-use App\Presenters\CreateProjectPresenter;
-use App\Requests\CreateRequest;
-use App\Services\CreateService;
-use Cag\Cag;
-use Cag\Factory\Model\StructureModelFactory;
-use Cag\Factory\Response\CreateProjectResponseFactory;
-use Cag\Services\ComposerService;
-use Cag\Services\FileService;
-use Cag\Services\FolderService;
-use Cag\Services\StructureService;
-use Cag\UseCases\CreateProjectUseCase;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class DependencyServiceProvider extends AbstractServiceProvider
@@ -33,32 +22,7 @@ class DependencyServiceProvider extends AbstractServiceProvider
      */
     public function provides(string $id): bool
     {
-        $services = [
-            'createProjectPresenter',
-            CreateProjectPresenter::class,
-            'createRequest',
-            CreateRequest::class,
-            'createService',
-            CreateService::class,
-            'dependencyInjection',
-            DependencyInjection::class,
-            'cag',
-            Cag::class,
-            'createProjectUseCase',
-            CreateProjectUseCase::class,
-            'fileService',
-            FileService::class,
-            'folderService',
-            FolderService::class,
-            'structureService',
-            StructureService::class,
-            'composerService',
-            ComposerService::class,
-            'structureModelFactory',
-            StructureModelFactory::class,
-            'createProjectResponseFactory',
-            CreateProjectResponseFactory::class
-        ];
+        $services = [];
 
         return in_array($id, $services);
     }
@@ -67,42 +31,5 @@ class DependencyServiceProvider extends AbstractServiceProvider
      * @return void
      */
     public function register(): void
-    {
-        $this->getContainer()->add(DependencyInjection::class);
-        $this->getContainer()->add(CreateProjectPresenter::class);
-        $this->getContainer()->add(CreateRequest::class);
-        $this->getContainer()
-            ->add(CreateService::class)
-            ->addArgument(DependencyInjection::class);
-        $this->getContainer()
-            ->add(Cag::class)
-            ->addArgument(DependencyInjection::class);
-
-        $this->getContainer()
-            ->add(FileService::class);
-        $this->getContainer()
-            ->add(FolderService::class);
-        $this->getContainer()
-            ->add(StructureService::class)
-            ->addArgument(FileService::class)
-            ->addArgument(FolderService::class);
-
-        $this->getContainer()
-            ->add(ComposerService::class);
-
-        $this->getContainer()
-            ->add(StructureModelFactory::class)
-            ->addArgument(FileService::class)
-            ->addArgument(FolderService::class);
-
-        $this->getContainer()
-            ->add(CreateProjectResponseFactory::class);
-
-        $this->getContainer()
-            ->add(CreateProjectUseCase::class)
-            ->addArgument(StructureService::class)
-            ->addArgument(ComposerService::class)
-            ->addArgument(StructureModelFactory::class)
-            ->addArgument(CreateProjectResponseFactory::class);
-    }
+    {}
 }
