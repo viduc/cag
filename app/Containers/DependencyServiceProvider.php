90dfbfc4c42c2a2c33a1b408f34824ee89580971
t.fleury <t.fleury@gammasoftware.fr>
2023-02-25 15:46:11 +0100

    refacto

--- app/Containers/DependencyServiceProvider.php
+++ app/Containers/DependencyServiceProvider.php
@@ -11,10 +11,23 @@ declare(strict_types=1);
 
 namespace App\Containers;
 
+use App\Presenters\CreateProjectPresenter;
+use App\Requests\CreateRequest;
+use App\Services\CreateService;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class DependencyServiceProvider extends AbstractServiceProvider
 {
+     const SERVICES = [
+        'createProjectPresenter',
+        CreateProjectPresenter::class,
+        'createRequest',
+        CreateRequest::class,
+        'createService',
+        CreateService::class,
+        'dependencyInjection',
+        DependencyInjection::class,
+    ];
     /**
      * @param string $id
      *
@@ -22,14 +35,21 @@ class DependencyServiceProvider extends AbstractServiceProvider
      */
     public function provides(string $id): bool
     {
-        $services = [];
 
-        return in_array($id, $services);
+
+        return in_array($id, self::SERVICES);
     }
 
     /**
      * @return void
      */
     public function register(): void
-    {}
+    {
+        $this->getContainer()->add(DependencyInjection::class);
+        $this->getContainer()->add(CreateProjectPresenter::class);
+        $this->getContainer()->add(CreateRequest::class);
+        $this->getContainer()
+            ->add(CreateService::class)
+            ->addArgument(DependencyInjection::class);
+    }
 }
